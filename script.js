aac4b24b8635e4af8661f1c415ff22432f6aebd2
maoloape <151963939+maoloape@users.noreply.github.com>
2026-06-02 15:08:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,181 @@
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbztWWuLrvF7BeaLE_EhKVav6K0Nm1ecjProwGaUx5qlacrAqvnFB8vPLHIGM_oVky9EIg/exec";
+
+const app = document.getElementById("app");
+const progress = document.getElementById("progress");
+
+const dataForm = {
+    tema:"",
+    makanan:"",
+    baju:"",
+    jam:""
+};
+
+const noBtn = document.getElementById("noBtn");
+
+function moveButton(){
+    const maxX = window.innerWidth - noBtn.offsetWidth;
+    const maxY = window.innerHeight - noBtn.offsetHeight;
+
+    noBtn.style.position = "fixed";
+    noBtn.style.left = Math.random() * maxX + "px";
+    noBtn.style.top = Math.random() * maxY + "px";
+}
+
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+
+document.getElementById("yesBtn").addEventListener("click", showTema);
+
+function showTema(){
+
+    progress.innerHTML = "1 / 4";
+
+    app.innerHTML = `
+        <h1>YEAYYYY 🥰🥰🥰<br>Tema mainnya mau apa ayang?</h1>
+
+        <button class="primary option" onclick="pilihTema('Alam')">🌳 Alam</button>
+        <button class="primary option" onclick="pilihTema('Jalan Jauh')">🚗 Jalan-jalan Jauh</button>
+        <button class="primary option" onclick="pilihTema('Cafe')">☕ Cafe</button>
+        <button class="primary option" onclick="pilihTema('Mall')">🛍️ Mall</button>
+        <button class="primary option" onclick="pilihTema('Binatang')">🐱 Binatang</button>
+    `;
+}
+
+window.pilihTema = function(tema){
+
+    dataForm.tema = tema;
+
+    progress.innerHTML = "2 / 4";
+
+    app.innerHTML = `
+        <h1>Mau makan apaa ayang? 😋</h1>
+
+        <button class="primary option" onclick="pilihMakanan('Ayam')">🍗 Ayam</button>
+        <button class="primary option" onclick="pilihMakanan('Ikan')">🐟 Ikan</button>
+        <button class="primary option" onclick="pilihMakanan('Seafood')">🦐 Seafood</button>
+        <button class="primary option" onclick="pilihMakanan('Pasta')">🍝 Pasta</button>
+        <button class="primary option" onclick="pilihMakanan('Burger')">🍔 Burger</button>
+        <button class="primary option" onclick="pilihMakanan('Mie')">🍜 Mie</button>
+        <button class="primary option" onclick="pilihMakanan('Nanti aja deh pas Ketemu')">Nanti aja deh pas Ketemu</button>
+    `;
+}
+
+window.pilihMakanan = function(makanan){
+
+    dataForm.makanan = makanan;
+
+    progress.innerHTML = "3 / 4";
+
+    app.innerHTML = `
+        <h1>Pake baju apa ayang? 👗</h1>
+
+        <input
+            id="baju"
+            placeholder="Contoh: Dress Hitam 🖤"
+        >
+
+        <br>
+
+        <button
+            class="primary"
+            onclick="simpanBaju()"
+        >
+            Lanjut ➜
+        </button>
+    `;
+}
+
+window.simpanBaju = function(){
+
+    dataForm.baju =
+        document.getElementById("baju").value;
+
+    progress.innerHTML = "4 / 4";
+
+    app.innerHTML = `
+        <h1>Kira-kira aku harus sampai tanggal dan jam berapa capaa? ⏰</h1>
+
+        <input type="date" id="tanggal">
+        <input type="time" id="jam">
+
+        <br>
+
+        <button
+            class="primary"
+            onclick="submitData()"
+        >
+            Kirim 🖤
+        </button>
+    `;
+}
+
+window.submitData = async function(){
+
+    dataForm.jam =
+        document.getElementById("jam").value;
+
+    try{
+
+        await fetch(APPS_SCRIPT_URL,{
+            method:"POST",
+            mode:"no-cors",
+            body: JSON.stringify(dataForm)
+        });
+
+    }catch(err){
+        console.error(err);
+    }
+
+    progress.innerHTML = "";
+
+    app.innerHTML = `
+        <div class="success">
+            YEAYYYY 🥰🥰🥰<br><br>
+            Makaciiiiii capaa 🖤<br><br>
+            Sampai ketemuuu Sabtuuu 🥰
+        </div>
+    `;
+}
+
+const loveContainer =
+    document.getElementById("loveContainer");
+
+function createLove(){
+
+    const love =
+        document.createElement("img");
+
+    love.src = "love.png";
+    love.classList.add("floating-love");
+
+    const size =
+        Math.random() * 25 + 20;
+
+    love.style.width = size + "px";
+
+    love.style.left =
+        Math.random() * window.innerWidth + "px";
+
+    const duration =
+        Math.random() * 4 + 5;
+
+    love.style.animationDuration =
+        duration + "s";
+
+    loveContainer.appendChild(love);
+
+    setTimeout(() => {
+        love.remove();
+    }, duration * 1000);
+}
+
+setInterval(() => {
+
+    const amount =
+        Math.floor(Math.random() * 3) + 1;
+
+    for(let i = 0; i < amount; i++){
+        createLove();
+    }
+
+}, 700);
